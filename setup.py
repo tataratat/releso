d9dfbcc9f2e6b9a3ac2fa895677cd904eef6030d
clemens.fricke <clemens.fricke@rwth-aachen.de>
2022-09-01 15:16:59 +0200

    install should now work again

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="SbSOvRL",
     version="1.1.0",
     author="Clemens Fricke",
-    author_email="clemens.fricke@ilsb.tuwien.ac.at",
+    author_email="clemens.fricke@tuwien.ac.at",
     description="""
     Spline based Shape Optimization via Reinforcement Learning is a framework
     and library which implements a new way to optimize geometries with 
@@ -18,18 +18,27 @@ setuptools.setup(
     The authors also wrote a seminar and master thesis during the development 
     of this library in which the underling functionality is given. If this is 
     needed please contact either clemens.fricke@rwth-aachen.de,
-    clemens.fricke@ilsb.tuwien.ac.at or wolff@cats.rwth-aachen.de.
+    clemens.fricke@tuwien.ac.at or wolff@cats.rwth-aachen.de.
     """,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
     project_urls={},
     classifiers=[
-        "Programming Language :: Python :: 3",
-        # "License :: OSI Approved :: MIT License",
-        # "Operating System :: OS Independent",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    license="MIT",
+    packages=setuptools.find_packages(),
     python_requires=">=3.8",
+    entry_points ={
+        'console_scripts': [
+            'sbsovrl = SbSOvRL.__main__:main'
+        ]
+    },
 )
