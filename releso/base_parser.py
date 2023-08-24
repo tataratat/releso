46df160c200e3ca98907edc730cefe1502d58066
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2023-08-24 16:51:11 +0200

    add base_parser tests

--- releso/base_parser.py
+++ releso/base_parser.py
@@ -28,9 +28,10 @@ class BaseParser(BaseModel):
     """Class parses the experiment definition and conducts the training.
 
     This class can be used to initialize the ReLeSO Framework from the command
-    line by reading in a json representation of the Spline based Shape
+    line by reading in a json representation of the RL based Shape
     Optimization problem which is to be solved via Reinforcement Learning.
     """
+
     #: Defining the verbosity of the training process and environment loading
     verbosity: Verbosity = Field(default_factory=Verbosity)
     #: Definition of the agent to be used during training and/or validation of
@@ -40,75 +41,80 @@ class BaseParser(BaseModel):
     #: case
     environment: Environment
     #: Number of timesteps the training process should run for
-    number_of_timesteps: conint(
-        ge=1
-    )
+    number_of_timesteps: conint(ge=1)
     #: Number of episodes the training process should run for. If given both
     #: timesteps and max episodes can stop the trainings progress.
     #: Default: None
-    number_of_episodes: Optional[conint(
-        ge=1
-    )] = None
-    #: Definition of the validation parameters
-    validation: Optional[Validation]
-    #: Number of environments to train in parallel
-    n_environments: Optional[conint(ge=1)]
-    # : Should training parameters be normalized to the number of environments
+    number_of_episodes: Optional[conint(ge=1)] = None
+    #: Definition of the validation . Defaults to None.
+    validation: Optional[Validation] = None
+    #: Number of environments to train in parallel. Defaults to None.
+    n_environments: Optional[conint(ge=1)] = 1
+    # : Should training parameters be normalized to the number of environments?
+    # : Defaults to False.
     normalize_training_values: bool = False
     #: Should the multi environment be run sequentially (True) or with multi
-    #: processing (False)
+    #: processing (False). Defaults to False.
     multi_env_sequential: bool = False
 
     # internal objects
     #: Holds the trainable agent for the RL use case. The
     #: ReLeSO.base_parser.BaseParser.agent defines the type and parameters of
     #: the agent this is the actual trainable agent.
-    _agent: Optional[BaseAlgorithm] = PrivateAttr(
-        default=None
-    )
+    _agent: Optional[BaseAlgorithm] = PrivateAttr(default=None)
 
-    def __init__(__pydantic_self__, **data: Any) -> None:
+    def __init__(self, **data: Any) -> None:
         """Constructor of the base parser.
 
         Initializes the class correctly and also adds the correct logger to all
         subclasses.
         """
         super().__init__(**data)
-        __pydantic_self__.set_logger_name_recursively(
-            __pydantic_self__.verbosity._environment_logger
-        )
+        self.set_logger_name_recursively(self.verbosity._environment_logger)
 
     def learn(self) -> None:
-        """Starts the training that is specified in the loaded json file."""
+        """Starts the training that is specified in the loaded json file.
+
+        Successive calls to this functions does not train the agent further
+        but reinitialize the agent.
+        """
         train_env: Optional[VecEnv] = None
         validation_environment = self._create_validation_environment()
-        normalizer_divisor = 1 if self.n_environments is None or not \
-            self.normalize_training_values else self.n_environments
+        normalizer_divisor = (
+            1
+            if self.n_environments is None
+            or not self.normalize_training_values
+            else self.n_environments
+        )
         if self.n_environments and self.n_environments > 1:
             env_create_list = []
             for idx in range(self.n_environments):
-                env_create_list.append(self._create_new_environment(
-                    self.get_logger().name+f"_{idx}"))
+                env_create_list.append(
+                    self._create_new_environment(
+                        self.get_logger().name + f"_{idx}"
+                    )
+                )
             if self.multi_env_sequential:
                 train_env = DummyVecEnv(env_create_list)
             else:
                 train_env = SubprocVecEnv(env_create_list)
         else:
             train_env = DummyVecEnv(
-                [lambda: self.environment.get_gym_environment()])
+                [lambda: self.environment.get_gym_environment()]
+            )
 
         self._agent = self.agent.get_agent(train_env, normalizer_divisor)
         self.get_logger().info(f"Agent is of type {type(self._agent)}")
         callbacks = [
             EpisodeLogCallback(
                 episode_log_location=self.save_location / "episode_log.csv",
-                verbose=1
+                verbose=1,
             ),
         ]
         if self.number_of_episodes is not None:
             num = self.number_of_episodes
             if self.normalize_training_values:
-                num = int(num/normalizer_divisor)
+                num = int(num / normalizer_divisor)
             callbacks.append(StopTrainingOnMaxEpisodes(max_episodes=num))
 
         if self.validation:
@@ -117,7 +123,7 @@ class BaseParser(BaseModel):
                     self.validation.get_callback(
                         validation_environment.get_gym_environment(),
                         save_location=self.save_location,
-                        normalizer_divisor=normalizer_divisor
+                        normalizer_divisor=normalizer_divisor,
                     )
                 )
 
@@ -135,25 +141,7 @@ class BaseParser(BaseModel):
         self.save_model()
         self.evaluate_model(validation_environment)
 
-    def export_spline(self, file_name: str) -> None:
-        """Exports the current spline to the specified location.
-
-        Args:
-            file_name (str): Path to the location where the spline should be
-            stored at.
-        """
-        self.environment.export_spline(file_name)
-
-    def export_mesh(self, file_name: str) -> None:
-        """Exports the current mesh to the specified location.
-
-        Args:
-            file_name (str): Path to the location where the mesh should be
-            stored at.
-        """
-        self.environment.export_mesh(file_name)
-
-    def save_model(self, file_name: Optional[str] = None) -> None:
+    def save_model(self, file_name: Optional[str] = None) -> str:
         """Save the state of the agent.
 
         Saves the current agent to the specified location or to a default
@@ -163,18 +151,20 @@ class BaseParser(BaseModel):
             file_name (Optional[str]): Path where the agent is saved to. If
             None will see if json had a save location if also not given will
             use a default location. Defaults to None.
+
+        Returns:
+            str: Path where the agent was saved to.
         """
+        if self._agent is None:
+            raise RuntimeError("Please train the agent first.")
         if file_name is None:
             path = pathlib.Path(self.save_location)
-        elif file_name is not None:
-            path = pathlib.Path(file_name)
         else:
-            path = pathlib.Path(
-                f"default_model_safe_location/"
-                f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-            )
+            path = pathlib.Path(file_name)
         path.parent.mkdir(parents=True, exist_ok=True)
-        self._agent.save(path / "model_end.save")
+        save_path = path / "model_end.save"
+        self._agent.save(save_path)
+        return save_path
 
     def evaluate_model(
         self,
@@ -204,20 +194,24 @@ class BaseParser(BaseModel):
                 validation_env = self._create_validation_environment(True)
             if self._agent is None:
                 self._agent = self.agent.get_agent(
-                    validation_env.get_gym_environment())
+                    validation_env.get_gym_environment()
+                )
             reward, episode_length = self.validation.end_validation(
                 agent=self._agent,
-                environment=validation_env.get_gym_environment()
+                environment=validation_env.get_gym_environment(),
             )
             reward_array = np.array(reward)
             mean_reward = reward_array.mean()
             reward_std = reward_array.std()
-            log_str = f"The end validation had the following results: "
-            f"mean_reward = {mean_reward}; reward_std = {reward_std}"
+            log_str = (
+                f"The end validation had the following results: "
+                f"mean_reward = {mean_reward}; reward_std = {reward_std}"
+            )
             self.get_logger().info(log_str)
             self.get_logger().info(f"The reward per episode was: {reward}.")
             self.get_logger().info(
-                f"The length per episode was: {episode_length}")
+                f"The length per episode was: {episode_length}"
+            )
         elif throw_error_if_None:
             raise ValidationNotSet()
 
@@ -228,16 +222,18 @@ class BaseParser(BaseModel):
             logger_name (str): name of the logger used for the rl_logger used
             for this specific environment.
         """
+
         def _init():
             c_env = deepcopy(self.environment)
             logging_information = {
                 "logger_name": logger_name,
                 "log_file_location": self.verbosity.logfile_location,
-                "logging_level": self.verbosity.environment
+                "logging_level": self.verbosity.environment,
             }
             return c_env.get_gym_environment(
                 logging_information=logging_information
             )
+
         return _init
 
     def _create_validation_environment(
@@ -275,8 +271,7 @@ class BaseParser(BaseModel):
         )
         validation_environment._id = None
         validation_environment.set_validation(
-            **self.validation.get_environment_validation_parameters(
-                self.save_location)
+            **self.validation.get_environment_validation_parameters()
         )
 
         return validation_environment
